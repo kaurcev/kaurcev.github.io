2a19996973aeea62e44e4fdef78337d808bef95a
kaurcev <skaurcev2004@yandex.ru>
2023-04-07 19:33:17 +0600

    fixed

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainPage from './MainPage';
-import ErrorPage from './ErrorPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-switch(window.location.pathname){
-  case("/"): active(<MainPage />); break;
-  default: active(<ErrorPage />); break;
-}
-
-function active(asd){
-  root.render(
-    <React.StrictMode>
-    {asd}
-  </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+  <MainPage />
+</React.StrictMode>
+);
 
 reportWebVitals();
