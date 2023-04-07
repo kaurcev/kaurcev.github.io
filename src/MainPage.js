747832d325b06e0f96e14b3e2c17aa5b6619ba01
kaurcev <skaurcev2004@yandex.ru>
2023-04-07 16:17:39 +0600

    update

---
+++ src/MainPage.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './MainPage.css';
+
+function MainPage() {
+    return ( 
+        <div 
+        className='hello'
+        >
+        <h1>Добро пожаловать</h1>
+        <p>На данный момент идёт перенос сайта на React.JS</p>
+        </div>
+    );
+}
+
+export default MainPage;
