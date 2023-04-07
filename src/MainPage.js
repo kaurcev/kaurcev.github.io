350014c50cff655a3ce29d4ea71449f576e7d1e4
kaurcev <skaurcev2004@yandex.ru>
2023-04-07 19:10:40 +0600

    update

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './MainPage.css';
 
+const off = "asd"
+
 function MainPage() {
     return ( 
+        <main>
         <div 
         className='hello'
         >
         <h1>Добро пожаловать</h1>
         <p>На данный момент идёт перенос сайта на React.JS</p>
         </div>
-        
+        <div>
+            <h2>Что планируется?</h2>
+            <p><i class="fa fa-cogs" aria-hidden="true"></i> Данное нововведение позволит повысить качество работы сайта и обеспечить новые функции.</p>
+            <p><i class="fa fa-paint-brush" aria-hidden="true"></i>Динаминчный контент будет оформлен в собственном UI интерфейсе</p>
+            <h3>Новая система ограничений</h3>
+            <p><i class="fa fa-ban" aria-hidden="true"></i> Планируется новая система ораничений, которая позволит работать с пользователями более детально</p>
+            <p id='IPCheck'>Система IPChecker {off}</p>
+        </div>
+        </main>
     );
 }
+
 
 export default MainPage;
